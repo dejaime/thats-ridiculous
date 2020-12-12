75b546e3087dd253bd6335542e12c1ee5bc6becf
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-12 02:28:02 -0200

    adding basic test player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    //Inspector Variables
     [SerializeField]
     float moveSpeed;
 
+    //Components
     Rigidbody rigidBody;
 
-    // Start is called before the first frame update
-    void Start()
+    //Control
+    Vector3 movementDirection;
+
+    private void Awake()
     {
         rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        float hMovement = Input.GetAxisRaw("Horizontal");
+        float vMovement = Input.GetAxisRaw("Vertical");
+
+        movementDirection = (hMovement * transform.right + vMovement * transform.forward).normalized;
+    }
+
+    private void FixedUpdate() {
+        Move();
+    }
+
+
+    private void Move () {
+        rigidBody.velocity = movementDirection * moveSpeed * Time.deltaTime;
     }
 }
