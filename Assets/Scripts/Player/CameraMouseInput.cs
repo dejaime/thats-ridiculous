3d5dbf16e76f8ba8b2c4cd2e94fddad5906b0600
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-12 03:34:57 -0200

    basic camera mouse controls

---
+++ Assets/Scripts/Player/CameraMouseInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMouseInput : MonoBehaviour
+{
+    [SerializeField]
+    float xSensitivity = 0.35f;
+
+    [SerializeField]
+    float ySensitivity = 0.35f;
+
+    [SerializeField]
+    float minX = -60f;
+
+    [SerializeField]
+    float maxX = 60f;
+
+    Vector2 rotation;
+
+
+    private void Awake() {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        rotation = new Vector2(
+            Input.GetAxis("Mouse Y") * xSensitivity,
+            Input.GetAxis("Mouse X") * ySensitivity
+        );
+
+        transform.localEulerAngles += new Vector3 (
+            -Mathf.Clamp(rotation.x, minX, maxX),
+            rotation.y,
+            0
+        );
+    }
+}
