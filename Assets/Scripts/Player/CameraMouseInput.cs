05ae445a55f6f1744539dc4e55da2643139a7c60
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-12 18:41:08 -0200

    Dealing with euler angles not going negative

--- Assets/Scripts/Player/CameraMouseInput.cs
+++ Assets/Scripts/Player/CameraMouseInput.cs
@@ -21,28 +21,37 @@ public class CameraMouseInput : MonoBehaviour
 
     Vector2 rotation;
 
+    //This is used to solve a problem where the angle circles back from 0 to 359.999~ when moving on the negative direction.
+    float verticalEulerAngle;
 
-    private void Awake() {
+    private void Awake()
+    {
         Cursor.lockState = CursorLockMode.Locked;
+
+        //Get the initial camera angle along the X axis (vertical [up/down] angle)
+        verticalEulerAngle = cameraTransform.localEulerAngles.x;
+        //Offset min and max by the initial value
+        minX += verticalEulerAngle;
+        maxX += verticalEulerAngle;
+
         Debug.Assert(cameraTransform != null, "Player camera transform reference is NULL. This is an error.");
     }
 
 
     void Update()
     {
-        rotation = new Vector2(
-            Input.GetAxis("Mouse Y") * verticalSensitivity,
-            Input.GetAxis("Mouse X") * horizontalSensitivity
-        );
-
-        transform.localEulerAngles += new Vector3 (
+        transform.localEulerAngles += new Vector3(
             0,
-            rotation.y,
+            Input.GetAxis("Mouse X") * horizontalSensitivity,
             0
         );
 
-        cameraTransform.localEulerAngles += new Vector3 (
-            -Mathf.Clamp(rotation.x, minX, maxX),
+        
+        verticalEulerAngle -= Input.GetAxis("Mouse Y") * verticalSensitivity;
+        verticalEulerAngle = Mathf.Clamp(verticalEulerAngle, minX, maxX);
+
+        cameraTransform.localEulerAngles = new Vector3(
+            verticalEulerAngle,
             0,
             0
         );
