cb782334c8dd83166de5f35f16130666289225a8
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-13 04:25:29 -0200

    Simple generic helper gizmo to see objects with no rendering

---
+++ Assets/Scripts/SimpleGizmo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleGizmo : MonoBehaviour
+{
+    [SerializeField]
+    Color color = Color.blue;
+
+    [SerializeField]
+    float radius = 1f;
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawSphere(transform.position, radius);
+    }
+}
