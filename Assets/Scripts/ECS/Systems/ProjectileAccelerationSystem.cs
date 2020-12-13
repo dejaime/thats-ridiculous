53f16f20c5286b4d46d50cd9688398128ac24bef
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-13 18:36:31 -0200

    Simple projectile acceleration system

---
+++ Assets/Scripts/ECS/Systems/ProjectileAccelerationSystem.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using UnityEngine;
+
+public class ProjectileAccelerationSystem : JobComponentSystem {
+		protected override JobHandle OnUpdate(JobHandle inputDeps) {
+				float deltaTime = Time.DeltaTime;
+
+				Entities.ForEach((ref ProjectileSpeedData speedData, in ProjectileAccelerationData accelerationData) => {
+						speedData.XZSpeed += accelerationData.XZAcceleration;
+				}).Run();
+
+				return inputDeps;
+		}
+}
