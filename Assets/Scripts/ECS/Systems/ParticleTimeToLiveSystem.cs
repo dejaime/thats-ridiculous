e91d0e10b3f4133b0a637b3e9078018fd9ed9275
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-13 19:37:25 -0200

    Adding DeleteEntityTag on TTL system

--- Assets/Scripts/ECS/Systems/ParticleTimeToLiveSystem.cs
+++ Assets/Scripts/ECS/Systems/ParticleTimeToLiveSystem.cs
@@ -6,9 +6,17 @@ using Unity.Collections;
 public class ProjectileTimeToLiveSystem : SystemBase {
 		protected override void OnUpdate() {
 				float deltaTime = Time.DeltaTime;
+
+                EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
                 
 				Entities.WithAll<ProjectileTimeToLiveData>().ForEach((Entity entity, ref ProjectileTimeToLiveData ttlData) => {
 						ttlData.timeToLive -= deltaTime;
+                        if (ttlData.timeToLive < 0) {
+                            commandBuffer.AddComponent<DeleteEntityTag>(entity);
+                        }
 				}).Run();
+
+                commandBuffer.Playback(EntityManager);
+                commandBuffer.Dispose();
 		}
 }
