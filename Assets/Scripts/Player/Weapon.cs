19a7abc11916f00a9d542b9b9b12344c1dd5ffcd
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-13 04:41:49 -0200

    basic weapon information, to be continued

---
+++ Assets/Scripts/Player/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+    [SerializeField]
+    float baseProjectileCount = 10;
+
+    [SerializeField]
+    int shotsPerRow = 20;
+
+    [SerializeField]
+    int firingSpreadEulerAngle = 45;
+
+    [SerializeField]
+    float cooldown = 1;
+
+    
+}
