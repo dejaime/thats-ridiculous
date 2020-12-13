7c96859cf76131d473031bfcc35e9d4cd3c03c65
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-13 18:36:12 -0200

    Simple projectile movement system

---
+++ Assets/Scripts/ECS/Systems/ProjectileMovementSystem.cs
@@ -0,0 +1,14 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Physics;
+using UnityEngine;
+
+public class ProjectileMovementSystem : JobComponentSystem {
+		protected override JobHandle OnUpdate(JobHandle inputDeps) {
+				Entities.ForEach((ref PhysicsVelocity velocity, in ProjectileSpeedData projectileData) => {
+						velocity.Linear.xz = projectileData.XZSpeed;
+				}).Run();
+				return inputDeps;
+		}
+}
