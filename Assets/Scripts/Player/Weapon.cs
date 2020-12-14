b4679cf461321edb9cf6fadaef1efd61a8f6bb27
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-14 19:58:32 -0200

    First projectile spawning weapon tests

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -1,19 +1,94 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Collections;
 
 public class Weapon : MonoBehaviour {
-    [SerializeField]
-    float baseProjectileCount = 10;
+	[SerializeField]
+	CharacterController playerController;
 
-    [SerializeField]
-    int shotsPerRow = 20;
+	[SerializeField]
+	int baseProjectileCount = 10;
 
-    [SerializeField]
-    int firingSpreadEulerAngle = 45;
+	int additionalProjectiles = 0;
 
-    [SerializeField]
-    float cooldown = 1;
+	[SerializeField]
+	int shotsPerRow = 20;
 
-    
+	[SerializeField]
+	int firingSpreadEulerAngle = 45;
+
+	[SerializeField]
+	float projectileSpeedMultiplier = 100;
+
+	[SerializeField]
+	float projectileAccelerationMultiplier = 10;
+
+	[SerializeField]
+	float cooldown = 0.2f;
+
+	float timeSinceLastShot = 0f;
+
+	public GameObject projectilePrefab;
+	private Entity projectileEntity;
+
+	private EntityManager entityManager;
+	private BlobAssetStore blobAssetStore;
+
+	private void Awake() {
+		entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+		blobAssetStore = new BlobAssetStore();
+		GameObjectConversionSettings goConversionSettings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, blobAssetStore);
+		projectileEntity = GameObjectConversionUtility.ConvertGameObjectHierarchy(projectilePrefab, goConversionSettings);
+	}
+
+
+	private void Update() {
+		timeSinceLastShot -= Time.deltaTime;
+
+		if (timeSinceLastShot < 0) {
+			timeSinceLastShot += cooldown;
+			Shoot();
+		}
+	}
+
+
+	private void Shoot() {
+		EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
+
+		int totalProjectiles = baseProjectileCount + additionalProjectiles;
+
+		float eulerMinAngle = -45;
+		float eulerMaxAngle = 45;
+
+		float angleStep = (eulerMaxAngle - eulerMinAngle) / totalProjectiles;
+
+		for (int i = 0; i < totalProjectiles; ++i) {
+			Entity newProjectile = entityManager.Instantiate(projectileEntity);
+			InitialProjectileSpatialData spatialData = new InitialProjectileSpatialData {
+				spawnPosition = transform.position,
+				speed = {
+					z = transform.forward.z * projectileSpeedMultiplier + playerController.velocity.z/2,
+					x = transform.forward.x * projectileSpeedMultiplier + playerController.velocity.x/2,
+					y = 2
+				},
+				acceleration = { 
+					x = transform.forward.x * projectileAccelerationMultiplier,
+					z = transform.forward.z * projectileAccelerationMultiplier,
+					y = -3f
+				},
+			};
+			commandBuffer.AddComponent<InitialProjectileSpatialData>(newProjectile, spatialData);
+		}
+
+		commandBuffer.Playback(entityManager);
+		commandBuffer.Dispose();
+	}
+
+
+	private void OnDestroy() {
+		blobAssetStore.Dispose();
+	}
+
 }
