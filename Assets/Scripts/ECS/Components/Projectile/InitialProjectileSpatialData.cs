ca1f14d62acdecf87b2c4deb6c6213953c7d23a0
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-14 19:49:44 -0200

    adding a setup component to configure new projectiles

---
+++ Assets/Scripts/ECS/Components/Projectile/InitialProjectileSpatialData.cs
@@ -0,0 +1,9 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct InitialProjectileSpatialData : IComponentData {
+    public float3 spawnPosition;
+    public float3 speed;
+    public float3 acceleration;
+}
---
+++ Assets/Scripts/ECS/Systems/Projectile/InitialProjectileSpatialSystem.cs
@@ -0,0 +1,31 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Physics;
+using Unity.Transforms;
+
+public class InitialProjectileSpatialSystem : SystemBase {
+	protected override void OnUpdate() {
+		float deltaTime = Time.DeltaTime;
+
+		EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
+
+		Entities.ForEach((
+			Entity entity,
+			ref PhysicsVelocity velocity,
+			ref Translation translation,
+			ref ProjectileAccelerationData accelerationData,
+			in InitialProjectileSpatialData initialSpatialData) => {
+
+			commandBuffer.RemoveComponent<InitialProjectileSpatialData>(entity);
+
+			translation.Value = initialSpatialData.spawnPosition;
+			velocity.Linear = initialSpatialData.speed;
+			accelerationData.acceleration = initialSpatialData.acceleration;
+
+		}).Run();
+
+		commandBuffer.Playback(EntityManager);
+		commandBuffer.Dispose();
+	}
+}
