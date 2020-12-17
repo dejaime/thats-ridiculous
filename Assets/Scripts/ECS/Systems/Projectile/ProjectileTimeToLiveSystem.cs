dbe55b14bb4daf31b6803dccd0ed05e0be511638
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-17 04:56:14 -0200

    Fixing typo in file name

---
+++ Assets/Scripts/ECS/Systems/Projectile/ProjectileTimeToLiveSystem.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using Unity.Entities;
+using Unity.Collections;
+
+[AlwaysSynchronizeSystem]
+public class ProjectileTimeToLiveSystem : SystemBase {
+	protected override void OnUpdate() {
+		float deltaTime = Time.DeltaTime;
+
+		EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
+
+		Entities.WithAll<ProjectileTimeToLiveData, ProjectileData>().ForEach((Entity entity, ref ProjectileTimeToLiveData ttlData, ref ProjectileData projectileData) => {
+			ttlData.timeToLive -= deltaTime;
+			if (ttlData.timeToLive < 0 || projectileData.hitsLeft <= 0) {
+				commandBuffer.AddComponent<DeleteEntityTag>(entity);
+			}
+		}).Run();
+
+		commandBuffer.Playback(EntityManager);
+		commandBuffer.Dispose();
+	}
+}
