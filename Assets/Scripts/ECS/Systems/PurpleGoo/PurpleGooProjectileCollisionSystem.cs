30e9d37a1c4f49623b7bf27c94ef2e99e7f83199
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-17 05:10:09 -0200

    Projectile X Purple Cubes collision system

---
+++ Assets/Scripts/ECS/Systems/PurpleGoo/PurpleGooProjectileCollisionSystem.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using Unity.Burst;
+using Unity.Entities;
+using Unity.Physics;
+using Unity.Physics.Systems;
+
+[UpdateInGroup(typeof(FixedStepSimulationSystemGroup))]
+[UpdateAfter(typeof(EndFramePhysicsSystem))]
+public class CubeHitEvent : SystemBase {
+	BuildPhysicsWorld buildPhysicsWorldSystem;
+	StepPhysicsWorld stepPhysicsWorldSystem;
+	EntityQuery projectileGroup, purpleCubeGroup;
+
+	protected override void OnCreate() {
+		buildPhysicsWorldSystem = World.GetOrCreateSystem<BuildPhysicsWorld>();
+		stepPhysicsWorldSystem = World.GetOrCreateSystem<StepPhysicsWorld>();
+
+		projectileGroup = GetEntityQuery(new EntityQueryDesc {
+			All = new ComponentType[] { typeof(ProjectileData) }
+		});
+
+		purpleCubeGroup = GetEntityQuery(new EntityQueryDesc {
+			All = new ComponentType[] { typeof(PurpleGooCubeData) }
+		});
+	}
+
+	[BurstCompile]
+	struct CollisionCubeHitJob : ICollisionEventsJob {
+		public ComponentDataFromEntity<ProjectileData> innerProjectileGroup;
+		public ComponentDataFromEntity<PurpleGooCubeData> innerCubeGroup;
+
+		public void Execute(CollisionEvent collisionEvent) {
+			Entity entityA = collisionEvent.EntityA;
+			Entity entityB = collisionEvent.EntityB;
+
+			bool isBodyAProjectile = innerProjectileGroup.HasComponent(entityA);
+			bool isBodyBProjectile = innerProjectileGroup.HasComponent(entityB);
+
+			bool isBodyACube = innerCubeGroup.HasComponent(entityA);
+			bool isBodyBCube = innerCubeGroup.HasComponent(entityB);
+
+			Entity projectile, cube;
+			if (isBodyAProjectile && isBodyBCube) {
+				projectile = entityA;
+				cube = entityB;
+			} else if (isBodyBProjectile && isBodyACube) {
+				projectile = entityB;
+				cube = entityA;
+			} else {
+				//Not a hit, not cube X projectile
+				return;
+			}
+
+			ProjectileData projectileData = innerProjectileGroup[projectile];
+			PurpleGooCubeData cubeData = innerCubeGroup[cube];
+			if (projectileData.hitsLeft > 0 && cubeData.height > 0) {
+				cubeData.height -= 5f;
+				innerCubeGroup[cube] = cubeData;
+
+				projectileData.hitsLeft -= 1;
+				innerProjectileGroup[projectile] = projectileData;
+			}
+		}
+	}
+
+	private static void OnHit(Entity projectile, Entity cube, ComponentDataFromEntity<ProjectileData> innerProjectileGroup, ComponentDataFromEntity<PurpleGooCubeData> innerCubeGroup) {
+		
+	}
+
+	protected override void OnUpdate() {
+		if (projectileGroup.CalculateEntityCount() == 0) {
+			return;
+		}
+
+		Dependency = new CollisionCubeHitJob {
+			innerProjectileGroup = GetComponentDataFromEntity<ProjectileData>(),
+			innerCubeGroup = GetComponentDataFromEntity<PurpleGooCubeData>(),
+		}.Schedule(stepPhysicsWorldSystem.Simulation,
+			ref buildPhysicsWorldSystem.PhysicsWorld, Dependency);
+		
+		Dependency.Complete();
+	}
+}
