ab0bc61f8a9b26812df18c1542c02f1b0e183bd2
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-17 04:57:15 -0200

    Changing PurpleCube's collider's height along with scale

--- Assets/Scripts/ECS/Systems/PurpleGoo/PurpleGooHeightSystem.cs
+++ Assets/Scripts/ECS/Systems/PurpleGoo/PurpleGooHeightSystem.cs
@@ -1,5 +1,6 @@
+using UnityEngine;
 using Unity.Entities;
-using Unity.Jobs;
+using Unity.Mathematics;
 using Unity.Physics;
 using Unity.Transforms;
 
@@ -8,9 +9,22 @@ public class PurpleGooHeightSystem : SystemBase {
 		float deltaTime = Time.DeltaTime;
 
 		Entities
-		.WithAll<NonUniformScale, PurpleGooCubeData>()
-		.ForEach((ref NonUniformScale nonUniformScale, in PurpleGooCubeData cubeData) => {
+		.WithAll<NonUniformScale, PhysicsCollider, PurpleGooCubeData>()
+		.ForEach((ref NonUniformScale nonUniformScale, ref Translation translation, ref PhysicsCollider collider, in PurpleGooCubeData cubeData) => {
 			nonUniformScale.Value.y = cubeData.height;
+			//Look, I know... unsafe code is bad, ugly, and all
+			//		sorry all, rustaceans especially
+			//		there's no other way of modifying a collider at runtime...
+			unsafe {
+				Unity.Physics.BoxCollider* boxColliderPtr = (Unity.Physics.BoxCollider*)collider.ColliderPtr;
+				BoxGeometry geometry = boxColliderPtr->Geometry;
+				geometry.Size = new float3 {
+					x = geometry.Size.x,
+					z = geometry.Size.z,
+					y = cubeData.height > 0.01f ? cubeData.height : 0.01f
+				};
+				boxColliderPtr->Geometry = geometry;
+			}
 		}).Schedule();
 	}
 }
