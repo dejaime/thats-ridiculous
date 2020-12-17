ac8d34cb6f05a3c7e2777e6e54054fc012abb5a7
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-17 01:56:15 -0200

    Adding projectile data component

---
+++ Assets/Scripts/ECS/Components/Projectile/ProjectileData.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct ProjectileData : IComponentData {
+    public int hitsLeft;
+    public float scale;
+}
