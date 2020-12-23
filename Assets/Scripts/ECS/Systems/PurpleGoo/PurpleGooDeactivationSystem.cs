5f3a51345a7fe358d54eecfa79309958d375e95d
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2020-12-23 21:07:04 -0200

    Reactivating goo cubes when height > 0

--- Assets/Scripts/ECS/Systems/PurpleGoo/PurpleGooDeactivationSystem.cs
+++ Assets/Scripts/ECS/Systems/PurpleGoo/PurpleGooDeactivationSystem.cs
@@ -12,10 +12,18 @@ public class PurpleGooDeactivationSystem : SystemBase {
 		.WithAll<Translation, PurpleGooCubeData>()
 		.ForEach((ref Translation translation, in PurpleGooCubeData cubeData) => {
 			if (cubeData.height <= 0) {
+				//If cube height is zero or negative, move it out of the play area	
 				translation.Value = new float3 {
 					x = translation.Value.x,
 					z = translation.Value.z,
-					y = -100
+					y = -50
+				};
+			} else {
+				//Move it back to the play area otherwise (or keep it in)
+				translation.Value = new float3 {
+					x = translation.Value.x,
+					z = translation.Value.z,
+					y = 1
 				};
 			}
 		}).Schedule();
