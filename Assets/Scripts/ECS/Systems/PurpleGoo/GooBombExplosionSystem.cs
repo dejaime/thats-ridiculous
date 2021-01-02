2e4eadac3fee230acec71b28bf8248090b964cb9
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2021-01-02 15:33:36 -0200

    Bombs `explode` after hitting the ground

--- Assets/Scripts/ECS/Systems/PurpleGoo/GooBombExplosionSystem.cs
+++ Assets/Scripts/ECS/Systems/PurpleGoo/GooBombExplosionSystem.cs
@@ -1,16 +1,20 @@
 using Unity.Entities;
 using Unity.Collections;
+using Unity.Transforms;
 
 [AlwaysSynchronizeSystem]
 public class GooBombExplosionSystem : SystemBase {
 	protected override void OnUpdate() {
 		EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
+		GooCubeGrid grid = GooCubeGrid.Instance;
 
-		Entities.WithAll<GooBombData>().WithNone<DeleteEntityTag>().ForEach((Entity entity, in GooBombData bombData) => {
-			if (bombData.hitGround) {
-				commandBuffer.AddComponent<DeleteEntityTag>(entity);
+		Entities.WithAll<Translation, GooBombData>().WithNone<DeleteEntityTag>().ForEach((Entity entity, in Translation translation, in GooBombData bombData) => {
+			if (!bombData.hitGround) {
+				return;
 			}
-		}).Run();
+			commandBuffer.AddComponent<DeleteEntityTag>(entity);
+			grid.Explode(translation.Value.x, translation.Value.z, bombData.bombSize);
+		}).WithoutBurst().Run();
 
 		commandBuffer.Playback(EntityManager);
 		commandBuffer.Dispose();
