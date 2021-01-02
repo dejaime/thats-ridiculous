8b594851a797648c0cac3ce1d2ed06a7336f266a
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2021-01-02 15:34:05 -0200

    Bombers' bomb spawn system

--- Assets/Scripts/ECS/Systems/PurpleGoo/GooBomberDropSystem.cs
+++ Assets/Scripts/ECS/Systems/PurpleGoo/GooBomberDropSystem.cs
@@ -1,18 +1,42 @@
 using Unity.Entities;
+using Unity.Burst;
+using Unity.Mathematics;
 using Unity.Transforms;
 
 public class GooBomberDropSystem : SystemBase {
+
+	BeginInitializationEntityCommandBufferSystem entityCommandBufferSystem;
+
+	protected override void OnCreate() {
+		base.OnCreate();
+ 		entityCommandBufferSystem = World.GetOrCreateSystem<BeginInitializationEntityCommandBufferSystem>();
+	}
+
 	protected override void OnUpdate() {
 		float deltaTime = Time.DeltaTime;
+		EntityCommandBuffer.ParallelWriter commandBuffer = entityCommandBufferSystem.CreateCommandBuffer().AsParallelWriter();
 
 		Entities
 		.WithAll<GooBomberData, Translation>()
-		.ForEach((ref GooBomberData gooBomberData, in Translation translation) => {
+		.WithBurst(FloatMode.Default, FloatPrecision.Standard, true)
+		.ForEach((int entityInQueryIndex, ref GooBomberData gooBomberData, in Translation translation) => {
 			gooBomberData.timeSinceLastDrop += deltaTime;
 			if (gooBomberData.timeSinceLastDrop > gooBomberData.cooldown) {
 				gooBomberData.timeSinceLastDrop -= gooBomberData.cooldown;
-				//DROP
+				Entity bomb = commandBuffer.Instantiate(entityInQueryIndex, gooBomberData.bombPrefab);
+
+				Translation bombTranslation = new Translation();
+				bombTranslation.Value = new float3 {
+					x = translation.Value.x,
+					y = translation.Value.y,
+					z = translation.Value.z
+				};
+
+				commandBuffer.SetComponent<Translation>(entityInQueryIndex, bomb, bombTranslation);
 			}
-		}).Schedule();
+		}).ScheduleParallel();
+
+		entityCommandBufferSystem.AddJobHandleForProducer(Dependency);
 	}
+
 }
