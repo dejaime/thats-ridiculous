01c6944d4a76c8be156e917db77a2b223b25c07c
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2021-01-02 15:23:27 -0200

    Updating deactivation system to make use of the inactive tag

--- Assets/Scripts/ECS/Systems/PurpleGoo/PurpleGooDeactivationSystem.cs
+++ Assets/Scripts/ECS/Systems/PurpleGoo/PurpleGooDeactivationSystem.cs
@@ -1,31 +1,43 @@
-using UnityEngine;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Collections;
 using Unity.Physics;
 using Unity.Transforms;
 
 public class PurpleGooDeactivationSystem : SystemBase {
+
+	EndSimulationEntityCommandBufferSystem endSimulationEcbSystem;
+
+	protected override void OnCreate() {
+		base.OnCreate();
+		endSimulationEcbSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+	}
+
 	protected override void OnUpdate() {
-		float deltaTime = Time.DeltaTime;
+		EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
 
 		Entities
 		.WithAll<Translation, PurpleGooCubeData>()
-		.ForEach((ref Translation translation, in PurpleGooCubeData cubeData) => {
+		.ForEach((Entity entity, int entityInQueryIndex, ref Translation translation, ref PurpleGooCubeData cubeData) => {
 			if (cubeData.height <= 0) {
 				//If cube height is zero or negative, move it out of the play area	
-				translation.Value = new float3 {
-					x = translation.Value.x,
-					z = translation.Value.z,
-					y = -50
-				};
+				if (cubeData.active) {
+					cubeData.active = false;
+					translation.Value.y = -100f;
+					commandBuffer.AddComponent<InactiveGooCubeTag>(entity);
+				}
 			} else {
-				//Move it back to the play area otherwise (or keep it in)
-				translation.Value = new float3 {
-					x = translation.Value.x,
-					z = translation.Value.z,
-					y = 1
-				};
+				// Move it back to the play area otherwise (but only if still inactive)
+				if (!cubeData.active) {
+					cubeData.active = true;
+					translation.Value.y = 1;
+					commandBuffer.RemoveComponent<InactiveGooCubeTag>(entity);
+				}
 			}
-		}).Schedule();
+		}).Run();
+
+		commandBuffer.Playback(EntityManager);
+		commandBuffer.Dispose();
 	}
 }
+
