dd8e220c1001357d8610d4789be8e07ad02978c1
Dejaime Antônio de Oliveira Neto <contact@dejai.me>
2021-01-12 04:04:25 -0200

    Spawning bombers using the predefined positions

--- Assets/Scripts/BomberSpawner.cs
+++ Assets/Scripts/BomberSpawner.cs
@@ -1,20 +1,41 @@
 ﻿using System.Linq;
 using UnityEngine;
+using Unity.Collections;
+using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Transforms;
 
 public class BomberSpawner : MonoBehaviour {
 
 	[SerializeField]
-	BomberSpawnPointData[] bomberSpawns;
+	BomberSpawnData[] bomberSpawns;
 
+	[SerializeField]
+	GameObject bomberPrefab;
+
+	[SerializeField]
+	GameObject bombPrefab;
+
+	float defaultBomberHeight = 200f;
 	int currentSpawnIndex = 0;
 	float timeUntilNextBomber = 0;
-	float defaultBomberCooldown = 5;
-	float bomberCooldownRandomDelta = 5;
+	float defaultBomberCooldown = 0;
+	float bomberCooldownRandomDelta = 1;
 
-    Unity.Mathematics.Random rnd = new Unity.Mathematics.Random();
+
+	//ECS variables
+	private Entity bomberEntityTemplate;
+	private Entity bombEntityTemplate;
+	private EntityManager entityManager;
+	private BlobAssetStore blobAssetStore;
 
 	void Start() {
+		entityManager = World.DefaultGameObjectInjectionWorld.EntityManager;
+		blobAssetStore = new BlobAssetStore();
+		GameObjectConversionSettings goConversionSettings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, blobAssetStore);
+		bomberEntityTemplate = GameObjectConversionUtility.ConvertGameObjectHierarchy(bomberPrefab, goConversionSettings);
+		bombEntityTemplate = GameObjectConversionUtility.ConvertGameObjectHierarchy(bombPrefab, goConversionSettings);
+
 		ShuffleSpawnArray();
 	}
 
@@ -23,27 +44,63 @@ public class BomberSpawner : MonoBehaviour {
 		TrySpawnBomber();
 	}
 
+	private void OnDestroy() {
+		blobAssetStore.Dispose();
+	}
+
 	private void TrySpawnBomber() {
 		if (timeUntilNextBomber > 0)
 			return;
 
-        BomberSpawnPointData spawnData = bomberSpawns[currentSpawnIndex++];
+		timeUntilNextBomber += defaultBomberCooldown + UnityEngine.Random.Range(0f, 1f) * bomberCooldownRandomDelta;
 
-        //Spawn bomber using position system
+		SpawnBomberAt(bomberSpawns[currentSpawnIndex]);
 
-        if (currentSpawnIndex == bomberSpawns.Count()) {
-            ShuffleSpawnArray();
-            currentSpawnIndex = 0;
-        }
+		++currentSpawnIndex;
+		if (currentSpawnIndex == bomberSpawns.Count()) {
+			ShuffleSpawnArray();
+			currentSpawnIndex = 0;
+		}
 	}
 
+
+	private void SpawnBomberAt(BomberSpawnData spawnData) {
+		Debug.Log("Spawn at " + spawnData.spawnPositionTransform.position);
+		EntityCommandBuffer commandBuffer = new EntityCommandBuffer(Allocator.TempJob);
+
+		Entity bomber = entityManager.Instantiate(bomberEntityTemplate);
+		commandBuffer.SetComponent<Translation>(bomber, new Translation {
+			Value = new float3 {
+				x = spawnData.spawnPositionTransform.position.x,
+				z = spawnData.spawnPositionTransform.position.z,
+				y = defaultBomberHeight
+			}
+		});
+
+		commandBuffer.SetComponent<ConstantSpeedData>(bomber, new ConstantSpeedData {
+			constantSpeed = spawnData.direction
+		});
+
+		commandBuffer.SetComponent<GooBomberData>(bomber, new GooBomberData {
+			timeSinceLastDrop = spawnData.timeSinceLastDrop,
+			cooldown = spawnData.cooldown,
+			//bombSize = spawnData.bombSize,
+			bombSize = 300,
+			bombEntityTemplate = bombEntityTemplate
+		});
+
+		commandBuffer.Playback(entityManager);
+		commandBuffer.Dispose();
+	}
+
+
 	private void ShuffleSpawnArray() {
-		bomberSpawns = bomberSpawns.OrderBy(x => rnd.NextFloat() > 0.5f).ToArray();
+		bomberSpawns = bomberSpawns.OrderBy(x => UnityEngine.Random.Range(0f, 1f) > 0.5f).ToArray();
 	}
 }
 
 [System.Serializable]
-public struct BomberSpawnPointData {
+public struct BomberSpawnData {
 	public Transform spawnPositionTransform;
 	public float3 direction;
 	public float timeSinceLastDrop;
